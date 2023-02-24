f6c12dc2a93913214c5d937a10218eab0f7b4737
eMobilis <preciousmwangi34@gmail.com>
2023-02-24 11:58:38 +0300

    sql databases

---
+++ main.py
@@ -0,0 +1,19 @@
+#install peewee
+from peewee import *
+from os import path
+
+connection = path.dirname(path.realpath(__file__))
+db = SqliteDatabase(path.join(connection, "Chris.db"))
+
+#creating our first table
+
+class User(Model):
+    name = CharField()
+    email = CharField()
+    password = CharField()
+
+    class Meta:
+        database = db
+User.create_table(fail_silently=True)
+
+
